3a48fbf6b706af62f91585615e97313809472938
jeel narola <narolajeel29@gmail.com>
2024-09-16 08:56:47 +0530

    variables

---
+++ Variables/var.js
@@ -0,0 +1,33 @@
+// var a = 1;
+// var a = 2;
+// console.log(a) // No, Error
+
+// let a = 1;
+// let a = 2;
+// console.log(a) // Identifier 'a' has already been declared
+
+
+// Function
+
+// function test () {
+//     if (true){
+//         var x =10;
+//     }
+//     console.log(x) // not Scoped
+// }
+// test()
+
+// function test1 (){
+//     if(true){
+//         let y = 10;
+//     }
+//     console.log(y) // No Print Because block-scoped
+// }
+
+// const prompt = require('prompt-sync')();
+
+// let sign = prompt("What's your sign? ");
+// console.log(`Your sign is: ${sign}`);
+
+
+//  npm install prompt-sync
